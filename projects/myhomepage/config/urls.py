44510abab37b53ca3915c2c3d39f74b850ea19c3
HyemCha <chm6194@gmail.com>
2022-11-10 17:04:57 +0900

    '답변 개수 표시&로그인'
    로그인: django.contrib.auth앱의 LoginView사용하도록 설정
    LoginView가 registration이 아닌 common디렉터리의 템플릿을 참조할 수 있도록함

--- projects/myhomepage/config/urls.py
+++ projects/myhomepage/config/urls.py
@@ -15,8 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from pybo import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('pybo/', include('pybo.urls')),
+    path('common/', include('common.urls')),
+    path('', views.index, name='index'),  # '/' 에 해당되는 path
 ]
+
+# 로그인 성공후 이동하는 URL
+LOGIN_REDIRECT_URL = '/'
