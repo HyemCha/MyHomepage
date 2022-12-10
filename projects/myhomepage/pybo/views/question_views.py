293258eafd8285f9008a86b36f1641cf27fdf823
HyemCha <chm6194@gmail.com>
2022-12-11 03:12:15 +0900

    imageFilter 시도오오

--- projects/myhomepage/pybo/views/question_views.py
+++ projects/myhomepage/pybo/views/question_views.py
@@ -3,12 +3,15 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 
+from PIL import Image, ImageFilter
+
 from ..forms import QuestionForm
 from ..models import Question
 
 
 @login_required(login_url='common:login')
-def question_create(request):
+def question_create(request, check=None):
+    check = None
     if request.method == 'POST':
         # request.POST를 인수로 QuestionForm을 생성할 경우에는
         # request.POST에 담긴 subject, content 값이 QuestionForm의 subject, content 속성에 자동으로 저장되어 객체가 생성
@@ -16,8 +19,14 @@ def question_create(request):
         if form.is_valid():
             # commit=False는 임시 저장을 의미
             # question객체 리턴받음
+            # image1 = form.
+            # print(image1)
             question = form.save(commit=False)
             question.author = request.user
+            # print(question.imgfile)
+            # image1 = Image.open(question.imgfile)
+            # image1 = image1.filter(ImageFilter.BLUR)
+            # question.imgfile = image1.
             # create_date 설정. create_date 속성은 데이터 저장 시점에 생성해야 하는 값이므로 QuestionForm에 등록하여 사용하지 않는다.
             question.create_date = timezone.now()
             # form에 저장된 데이터로 Question 데이터를 저장하기 위한 코드
